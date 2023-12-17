ccf36f1e9a027045050e684afcb21633064a7b5f
guiihxyz <127353136+guiihxyz@users.noreply.github.com>
2023-12-17 20:07:24 -0300

    update

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -5,15 +5,27 @@ function contar() {
     let resultado = document.querySelector("#res")
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        alert("Faltam dados")
+        resultado.innerHTML = "Impossível contar"
     } else {
-        resultado.innerHTML = "Contando: "
-        let i = ini.value
-        let f = fim.value
-        let p = passo.value
+        resultado.innerHTML = "Contando:<br>"
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
 
-        for(let c = i; c <= f; c += p) {
-            resultado.innerHTML += `${c} \u{1F449}`
+        if (p <= 0) {
+            alert("Passo inválido! Considerando passo 1")
+            p = 1;
+            passo.value = 1;
         }
+        if (i < f) {
+            for(let c = i; c <= f; c += p) {
+                resultado.innerHTML += `${c} \u{1F449} `
+            }
+        } else {
+            for(let c = i; c >= f; c -= p) {
+                resultado.innerHTML += `${c} \u{1F449} `
+            }
+        }
+        resultado.innerHTML += `\u{1F3C1}`
     }
 }
