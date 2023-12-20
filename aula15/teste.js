7237d5d5053b242529b9d068633b6bb7c3c99747
guiihxyz <127353136+guiihxyz@users.noreply.github.com>
2023-12-19 21:10:28 -0300

    update

---
+++ aula15/teste.js
@@ -0,0 +1,11 @@
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for(let i = 0; i < num.length; i++) {
+    console.log(num[i])
+}
+
+console.log("------------------------")
+
+for(let i in num) {
+    console.log(num[i])
+}
---
+++ aula16/funcao001.js
@@ -0,0 +1,8 @@
+function parOuImpar(n) {
+    if (n % 2 == 0) {
+        return "par"
+    } else {
+        return "ímpar"
+    }
+}
+console.log(parOuImpar(2323))
